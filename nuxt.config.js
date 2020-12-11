5de1940b09ba2c2e573a6b2ae2e5fb599c6b791d
Cristian Lluis <cristian@lluis.ch>
2020-12-12 00:54:12 +0100

    Added a light color Theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,16 +61,16 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
+        light: {
+          primary: '#0288D1',
+          secondary: '0c6aa0',
+          accent: '#7C4DFF',
+          info: colors.lime.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          success: colors.teal.accent3
         }
       }
     }
