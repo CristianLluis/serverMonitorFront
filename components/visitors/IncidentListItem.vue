4a1dd673932ead81d2f857d9711b85a91b7c4077
Cristian Lluis <cristian@lluis.ch>
2020-12-12 01:06:59 +0100

    IncidentListImtem component
    On Index page a list of Incidents wil be fetched.
    This component is used to iterate over this list and
    create an incident item per entry in the list

---
+++ components/visitors/IncidentListItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <v-card
+      v-if="!hasNoIncidents"
+      elevation="6"
+      class="mb-4"
+      @click="$emit('click')"
+    >
+      <v-card-text>
+        <p class="text-h5">
+          {{ incident.title }}
+        </p>
+        <p class="text-h6">
+          {{ lastUpdate.description }}
+        </p>
+        <div class="d-flex justify-space-between">
+          <p>
+            {{ howLongAgo(lastUpdate.date) }}
+          </p>
+          <p>Status: {{ lastUpdate.status }}</p>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card class="mb-4" elevation="6" v-else>
+      <v-card-text>
+        No incidents :-)
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "Incident",
+
+  props: {
+    incident: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    hasNoIncidents: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    lastUpdate() {
+      return this.incident.update_set[0];
+    }
+  },
+
+  methods: {
+    howLongAgo: value => {
+      return moment.utc(value).fromNow();
+    }
+  }
+};
+</script>
+
+<style></style>
