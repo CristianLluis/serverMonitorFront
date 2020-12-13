05407fd0b47fe1af5b6696062a914bad8002cd72
Cristian Lluis <cristian@lluis.ch>
2020-12-13 20:14:15 +0100

    history component displays multiple incidents per day

--- components/visitors/HistoryView.vue
+++ components/visitors/HistoryView.vue
@@ -1,12 +1,19 @@
 <template>
   <v-card class="mb-4" elevation="6">
     <v-timeline dense>
-      <v-timeline-item v-for="(date, index) in dates" small :key="index">
+      <v-timeline-item
+        v-for="(date, index) in dates"
+        small
+        :key="index"
+        :icon="getIncidents(date).length >= 1 ? 'mdi-exclamation' : 'mdi-check'"
+        :color="getIncidents(date).length >= 1 ? 'warning' : 'success'"
+        fill-dot
+      >
         <div>
           {{ getDateAsString(date) }}
         </div>
-        <div>
-          <IncidentMsg :dateObject="checkIncident(date)" />
+        <div class="incidents ml-2">
+          <IncidentMsg :incidents="getIncidents(date)" />
         </div>
       </v-timeline-item>
     </v-timeline>
@@ -51,37 +58,34 @@ export default {
             status: "resolved"
           }
         ]
+      },
+      {
+        id: 3,
+        title: "Email Server Down",
+        end: "2020-12-12 19:15:12",
+        start: "2020-12-11 13:00:12",
+        update_set: [
+          {
+            description: "Could not send emails",
+            date: "2020-12-11 14:00:12",
+            status: "resolved"
+          }
+        ]
       }
     ],
     dates: []
   }),
 
   methods: {
-    getEndDatesAndTitleOfClosedInsidets() {
-      return this.closedIncidents.map(i => ({
-        date: moment(i.end)
+    getIncidents(date) {
+      return this.closedIncidents.filter(i =>
+        moment(i.end)
           .startOf("day")
-          .format(),
-        title: i.title
-      }));
+          .isSame(moment(date).startOf("day"))
+      );
     },
 
-    getDateAsString: date => moment(date).format("ddd, DD MMM YYYY"),
-
-    checkIncident(date) {
-
-      let dateMomentString = moment(date)
-        .startOf("day")
-        .format();
-
-      let incident = this.getEndDatesAndTitleOfClosedInsidets().filter(
-        i => i.date === dateMomentString
-      );
-      return {
-        hasIncident: incident.length >= 1 ? true : false,
-        incident: incident
-      };
-    }
+    getDateAsString: date => moment(date).format("ddd, DD MMM YYYY")
   },
 
   created() {
@@ -93,3 +97,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.incidents {
+  font-size: 0.75rem
+}
+</style>
--- components/visitors/general/IncidentMsg.vue
+++ components/visitors/general/IncidentMsg.vue
@@ -1,54 +1,47 @@
 <template>
   <div class="d-flex">
-    <v-icon
-      class="mr-3 align-self-start pt-1"
-      small
-      v-text="hasIncident ? 'mdi-alert-circle' : 'mdi-check-circle'"
-      :color="hasIncident ? 'warning' : 'success'"
-    ></v-icon>
-    <div v-if="hasIncident" class="grey--text text--darken-1 d-inlineBlock">
-      <div>
-        {{ incidentTitle }}
-      </div>
-      <div class="text-caption">
-        Resolved on: {{ incidentEndDate }}
+    <div v-if="!hasIncidents">
+      <span class="grey--text text--lighten-1">
+        No incidents
+      </span>
+    </div>
+    <div v-else>
+      <div v-for="incident in incidents" :key="incident.id" class="mt-1">
+        <div class="text--lighten-2">
+          {{incident.title}}
+        </div>
+        <div class="grey--text text-caption">
+          Resolved on: {{ getResolveDate(incident) }}
+        </div>
       </div>
     </div>
-    <span v-else class="grey--text text--lighten-1">
-      No incidents
-    </span>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
 
 export default {
   name: "IncidentMsg",
 
   props: {
-    dateObject: {
-      type: Object,
-      default: () => {}
+    incidents: {
+      type: Array,
+      default: () => []
     }
   },
 
   computed: {
-    hasIncident() {
-      return this.dateObject.hasIncident;
-    },
-    incidentTitle() {
-      return this.hasIncident ? this.dateObject.incident[0].title : ""
-    },
-    incidentEndDate(){
-      return this.hasIncident ? moment(this.dateObject.incident[0].date).format("d. MMM yy") : ""
-    },
+    hasIncidents() {
+      console.log(this.incidents);
+      return this.incidents.length >= 1;
+    }
+  },
+
+  methods: {
+    getResolveDate(incident) {
+      return moment(incident.end).format("dd. MMM yy");
+    }
   }
 };
 </script>
-
-<style scoped>
-  .d-inlineBlock{
-    display: inline-block;
-  }
-</style>
