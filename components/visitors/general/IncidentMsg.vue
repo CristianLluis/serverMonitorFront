784a09528e957acf56807d08bb2e1e581df88439
Cristian Lluis <cristian@lluis.ch>
2020-12-14 19:32:27 +0100

    make incidents in history view clickable
    routing to new incident detail component
    incident id is passed as param

--- components/visitors/general/IncidentMsg.vue
+++ components/visitors/general/IncidentMsg.vue
@@ -6,13 +6,21 @@
       </span>
     </div>
     <div v-else>
-      <div v-for="incident in incidents" :key="incident.id" class="mt-1">
-        <div class="text--lighten-2">
-          {{incident.title}}
-        </div>
-        <div class="grey--text text-caption">
-          Resolved on: {{ getResolveDate(incident) }}
-        </div>
+      <div
+        v-for="incident in incidents"
+        :key="incident.id"
+        class="mt-1"
+      >
+        <v-hover v-slot="{ hover }">
+          <v-card class="pa-1" @click="openIncidentDetails(incident)" :elevation="hover ? '2' : '0'">
+            <div class="grey--text text--darken-1">
+              {{ incident.title }}
+            </div>
+            <div class="text-caption grey--text">
+              Resolved on: {{ getResolveDate(incident) }}
+            </div>
+          </v-card>
+        </v-hover>
       </div>
     </div>
   </div>
@@ -33,7 +41,6 @@ export default {
 
   computed: {
     hasIncidents() {
-      console.log(this.incidents);
       return this.incidents.length >= 1;
     }
   },
@@ -41,6 +48,10 @@ export default {
   methods: {
     getResolveDate(incident) {
       return moment(incident.end).format("dd. MMM yy");
+    },
+
+    openIncidentDetails(incident) {
+      this.$router.push({ name: "incident-id", params: { id: incident.id } });
     }
   }
 };
